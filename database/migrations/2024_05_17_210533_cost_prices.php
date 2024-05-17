6bc23f19c3d544b9dcf1a300dd9228f22f7c5b21
DESKTOP-ATG2C05\Andrew <ab@agronews.com>
2024-05-18 02:18:47 +0300

    Ещё коммит

---
+++ database/migrations/2024_05_17_210533_cost_prices.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cost_prices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('date')->unsigned(); // поле для хранения даты в виде числа
+            $table->decimal('price', 8, 2); // поле для хранения цены
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
